b183e7624d3866e6d6d2cada09885158f4ed3542
rocket1 <jazzmongrel@gmail.com>
2018-01-26 10:47:34 -0800

    Added native map but not yet showing.

--- app/components/map-screen/map-screen.js
+++ app/components/map-screen/map-screen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import MapView from 'react-native-maps';
+import MapView from 'react-native-maps';
 import {StyleSheet, View, Text} from 'react-native';
 
 class MapScreen extends React.Component {
@@ -8,31 +8,65 @@ class MapScreen extends React.Component {
         title: 'Map',
     };
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            region: {
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            },
+            markers: [],
+        };
+    }
+
+    onRegionChange = (region) => {
+        this.setState({region});
+    };
+
+    onMapPress = (e) => {
+        this.setState({
+            markers: [
+                ...this.state.markers,
+                {
+                    coordinate: e.nativeEvent.coordinate,
+                    key: `marker_${this.state.markers.length}`,
+                },
+            ],
+        });
+    };
+
     render() {
         return (
-            <View style={styles.container}>
+            <View>
+                <Text>Maps!</Text>
+                <MapView
+                    region={this.state.region}
+                    onRegionChange={this.onRegionChange}
+                    onPress={this.onMapPress}>
 
-                {/*<MapView*/}
-                    {/*initialRegion={{*/}
-                        {/*latitude: 37.78825,*/}
-                        {/*longitude: -122.4324,*/}
-                        {/*latitudeDelta: 0.0922,*/}
-                        {/*longitudeDelta: 0.0421,*/}
-                    {/*}}*/}
-                {/*/>*/}
+                    {this.state.markers.map(marker => (
+                        <MapView.Marker
+                            coordinate={marker.coordinate}
+                            key={marker.key}
+                        />
+                    ))}
+
+                </MapView>
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+//
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+// });
 
 export default MapScreen;
 
