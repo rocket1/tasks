b0cf5a7b1c3d70a1b152c1d385dbe1373b9a77d2
rocket1 <jazzmongrel@gmail.com>
2018-02-06 10:42:15 -0800

    Better init screen behavior.

--- app/components/init-screen/init-screen.js
+++ app/components/init-screen/init-screen.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Button, ActivityIndicator} from 'react-native';
 import {loadInitRegion} from '../../redux/actions';
 import LocationService from "../location-service/location-service";
 import {DEBUG_HOME_COORDS} from "../common/constants";
+import {BG_COLOR} from "../common/styles-common";
 
 class ConnectedInitScreen extends React.Component {
 
@@ -20,7 +21,13 @@ class ConnectedInitScreen extends React.Component {
      * @param props
      */
     constructor(props) {
+
         super(props);
+
+        this.state = {
+            initDone: false
+        };
+
         this._initRegion();
     }
 
@@ -31,9 +38,17 @@ class ConnectedInitScreen extends React.Component {
     _initRegion() {
         (new LocationService).getCurrentRegion((region) => {
             this.props.loadInitRegion(region);
-            this.props.navigation.navigate('Tasks');
+            // setTimeout(() => {
+                this.setState({
+                    initDone: true
+                });
+            // }, 1000);
         });
     }
+
+    _handleTaskBtnClick = () => {
+        this.props.navigation.navigate('Tasks');
+    };
 
     /**
      *
@@ -41,10 +56,22 @@ class ConnectedInitScreen extends React.Component {
      */
     render() {
         return (
-            <View><Text>Loading tasks...</Text></View>
+            <View style={styles.mainView}>
+                {/*{!this.state.initDone && <ActivityIndicator size="large" color="#000000"/>}*/}
+                {this.state.initDone && <Button onPress={this._handleTaskBtnClick} title='My Tasks'/>}
+            </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    mainView: {
+        backgroundColor: '#fff',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
 
 const mapDispatchToProps = dispatch => {
     return {
